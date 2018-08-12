908bb540c733d2516b2ba0fdbc30fb1b83094641
Charpell <charpellumeh@gmail.com>
2018-08-12 12:12:46 +0100

    Use Images and other reuseable styling component

--- App/Components/Dashboard.js
+++ App/Components/Dashboard.js
@@ -1,11 +1,63 @@
 import React, { Component } from 'react'
-import { Text, View, StyleSheet } from 'react-native'
+import { Text, View, StyleSheet, Image, TouchableHighlight } from 'react-native'
 
 export class Dashboard extends Component {
+  makeBackground(btn) {
+    var obj = {
+      flexDirection: 'row',
+      alignSelf: 'stretch',
+      justifyContent: 'center',
+      flex: 1
+    };
+
+    if (btn === 0) {
+      obj.backgroundColor = '#48BBEC';
+    } else if (btn === 1) {
+      obj.backgroundColor = '#E77AAE';
+    } else {
+      obj.backgroundColor = '#758BF4';
+    }
+
+    return obj;
+  }
+  
+  goToProfile() {
+    console.log('Going to Profiles')
+  }
+
+  goToRepos() {
+    console.log('Going to Repos')
+  }
+
+  goToNotes() {
+    console.log('Going to Notes')
+  }
+  
   render() {
     return (
       <View style={styles.container}>
-        <Text> This is the Dashboard </Text>
+        <Image source={{uri: this.props.userInfo.avatar_url}} style={styles.image} />
+        <TouchableHighlight
+          style={this.makeBackground(0)}
+          onPress={this.goToProfile.bind(this)}
+          underlayColor='#88d4f5'
+        >
+          <Text style={styles.buttonText}> View Profile </Text>
+        </TouchableHighlight>
+        <TouchableHighlight
+          style={this.makeBackground(1)}
+          onPress={this.goToRepos.bind(this)}
+          underlayColor='#88d4f5'
+        >
+          <Text style={styles.buttonText}> View Repos </Text>
+        </TouchableHighlight>
+        <TouchableHighlight
+          style={this.makeBackground(2)}
+          onPress={this.goToNotes.bind(this)}
+          underlayColor='#88d4f5'
+        >
+          <Text style={styles.buttonText}> View Notes </Text>
+        </TouchableHighlight>
       </View>
     )
   }
