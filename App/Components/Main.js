d523314f95bb3a649368f0973c5a26abdfacf2ff
Charpell <charpellumeh@gmail.com>
2018-08-12 09:54:30 +0100

    State and Touchable Event

--- App/Components/Main.js
+++ App/Components/Main.js
@@ -1,11 +1,47 @@
 import React, { Component } from 'react'
-import { Text, View, StyleSheet } from 'react-native'
+import { Text, View, StyleSheet, TextInput, TouchableHighlight } from 'react-native'
 
 export default class Main extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: '',
+      isLoading: false,
+      error: false
+    }
+  }
+
+  handleChange(event) {
+    this.setState({
+      username: event.nativeEvent.text
+    })
+  }
+
+  handleSubmit() {
+    // update our indicatorIos spinner
+    this.setState({
+      isLoading: true
+    })
+    console.log('SUBMIT', this.state.username)
+    // fetch data from github
+    // reroute to the next, passing the github information
+  }
+
   render() {
     return (
       <View style={styles.mainContainer}>
-        <Text> Testing the Router </Text>
+        <Text style={styles.title}> Search for a Github User </Text>
+        <TextInput 
+          style={styles.searchInput}
+          value={this.state.username}
+          onChange={this.handleChange.bind(this)}
+          />
+        <TouchableHighlight
+          style={styles.button}
+          onPress={this.handleSubmit.bind(this)}
+          underlayColor="white" >
+          <Text style={styles.buttonText}> SEARCH </Text>
+        </TouchableHighlight>
       </View>
     )
   }
